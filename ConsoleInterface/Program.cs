87667862bcd61b4acc5e4703982784e10e365860
mykytko <nd235471@gmail.com>
2021-09-10 00:59:30 +0300

    Initial commit

---
+++ ConsoleInterface/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CringeForestLibrary;
+
+namespace ConsoleInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
